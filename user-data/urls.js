f88e01575352e69951873b9b73683b2c04a8d99b
Aditya Patel <ap2935@njit.edu>
2025-07-18 13:20:25 -0400

    Update urls.js
    level 1

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,5 +1,5 @@
-const githubUsername = "vinaysomawat";
-const mediumUsername = "vinaysomawat";
+const githubUsername = "AdityaPatel1068";
+const mediumUsername = "theadityapatel"; // or your actual Medium username
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
